e87afd7beff8eff3358b1aa58e8aa669b9c4de93
Service Web <sw@gssmail.com>
2024-03-14 11:29:23 -0500

    Commit changes in C:\Websites\Git\emarlow\5536\GCG_5536_Cust_XRef.sql

---
+++ GCG_5536_Cust_XRef.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5536_Cust_XRef
+ * ========================================== */
+CREATE TABLE "GCG_5536_Cust_XRef"(
+ "AS400_Customer" CHAR(7) NOT NULL ,
+ "GSS_Customer" CHAR(6) NOT NULL ,
+ "GSS_Customer_Name" CHAR(30));
+
+
