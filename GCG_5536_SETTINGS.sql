158a42f7cf4a1de0bc8fb9e5f5e8f5a4925c364a
Service Web <sw@gssmail.com>
2024-03-14 11:29:23 -0500

    Commit changes in C:\Websites\Git\emarlow\5536\GCG_5536_SETTINGS.sql

---
+++ GCG_5536_SETTINGS.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_5536_SETTINGS
+ * ========================================== */
+CREATE TABLE "GCG_5536_SETTINGS"(
+ "RemoteHost" CHAR(50),
+ "RemoteUser" CHAR(50),
+ "RemotePassword" CHAR(50),
+ "RemotePort" CHAR(50),
+ "RemotePath" CHAR(50));
+
+
