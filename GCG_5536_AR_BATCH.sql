7a8fbb9313c7e3b66e41fb1954b53dc65c60fa82
Service Web <sw@gssmail.com>
2024-03-14 11:29:23 -0500

    Commit changes in C:\Websites\Git\emarlow\5536\GCG_5536_AR_BATCH.sql

---
+++ GCG_5536_AR_BATCH.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_5536_AR_BATCH
+ * ========================================== */
+CREATE TABLE "GCG_5536_AR_BATCH"(
+ "GCG_AR_BATCH" CHAR(5) NOT NULL ,
+ "GSS_USER" CHAR(9),
+ "CREATED_ON" DATE);
+
+
